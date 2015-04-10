0eb6636990608173b04963b76b022b8713b2e639
Isaku Yamahata <isaku.yamahata@intel.com>
2015-04-10 14:14:45 -0700

    Revert "drop service instance rest resources and drop related codes"
    This reverts commit 885387a4da91e8ca113509ccabc0eb0b2569a78b.
    
    Conflicts:
    	tacker/db/migration/alembic_migrations/versions/4c31092895b8_remove_service_instance.py
    	tacker/db/vm/vm_db.py
    	tacker/extensions/servicevm.py
    	tacker/vm/plugin.py
    
    Change-Id: Idef94da39de90cc5bcc4e830a054083059844a04

--- tacker/vm/plugin.py
+++ tacker/vm/plugin.py
@@ -24,10 +24,12 @@ import inspect
 
 import eventlet
 from oslo_config import cfg
+from sqlalchemy.orm import exc as orm_exc
 
 from tacker.api.v1 import attributes
 from tacker.common import driver_manager
 from tacker.common import topics
+from tacker.db.vm import proxy_db  # noqa
 from tacker.db.vm import vm_db
 from tacker.extensions import servicevm
 from tacker.openstack.common import excutils
@@ -96,6 +98,59 @@ class ServiceVMMgmtMixin(object):
             device_dict, plugin=self, context=context, device=device_dict,
             kwargs=kwargs)
 
+    def mgmt_service_driver(self, context, device_dict, service_instance_dict):
+        return self._invoke(
+            device_dict, plugin=self, context=context, device=device_dict,
+            service_instance=service_instance_dict)
+
+    def mgmt_service_create_pre(self, context, device_dict,
+                                service_instance_dict):
+        return self._invoke(
+            device_dict, plugin=self, context=context, device=device_dict,
+            service_instance=service_instance_dict)
+
+    def mgmt_service_create_post(self, context, device_dict,
+                                 service_instance_dict):
+        return self._invoke(
+            device_dict, plugin=self, context=context, device=device_dict,
+            service_instance=service_instance_dict)
+
+    def mgmt_service_update_pre(self, context, device_dict,
+                                service_instance_dict):
+        return self._invoke(
+            device_dict, plugin=self, context=context, device=device_dict,
+            service_instance=service_instance_dict)
+
+    def mgmt_service_update_post(self, context, device_dict,
+                                 service_instance_dict):
+        return self._invoke(
+            device_dict, plugin=self, context=context, device=device_dict,
+            service_instance=service_instance_dict)
+
+    def mgmt_service_delete_pre(self, context, device_dict,
+                                service_instance_dict):
+        return self._invoke(
+            device_dict, plugin=self, context=context, device=device_dict,
+            service_instance=service_instance_dict)
+
+    def mgmt_service_delete_post(self, context, device_dict,
+                                 service_instance_dict):
+        return self._invoke(
+            device_dict, plugin=self, context=context, device=device_dict,
+            service_instance=service_instance_dict)
+
+    def mgmt_service_address(self, context, device_dict,
+                             service_instance_dict):
+        return self._invoke(
+            device_dict, plugin=self, context=context, device=device_dict,
+            service_instance=service_instance_dict)
+
+    def mgmt_service_call(self, context, device_dict, service_instance_dict,
+                          kwargs):
+        return self._invoke(
+            device_dict, plugin=self, context=context, device=device_dict,
+            service_instance=service_instance_dict, kwargs=kwargs)
+
 
 class ServiceVMPlugin(vm_db.ServiceResourcePluginDb, ServiceVMMgmtMixin):
     """ServiceVMPlugin which supports ServiceVM framework
@@ -136,6 +191,16 @@ class ServiceVMPlugin(vm_db.ServiceResourcePluginDb, ServiceVMMgmtMixin):
                       {'infra_driver': infra_driver,
                        'drivers': cfg.CONF.servicevm.infra_driver})
             raise servicevm.InvalidInfraDriver(infra_driver=infra_driver)
+
+        service_types = template.get('service_types')
+        if not attributes.is_attr_set(service_types):
+            LOG.debug(_('service type must be specified'))
+            raise servicevm.ServiceTypesNotSpecified()
+        for service_type in service_types:
+            # TODO(yamahata):
+            # framework doesn't know what services are valid for now.
+            # so doesn't check it here yet.
+            pass
 
         return super(ServiceVMPlugin, self).create_device_template(
             context, device_template)
@@ -206,12 +271,14 @@ class ServiceVMPlugin(vm_db.ServiceResourcePluginDb, ServiceVMMgmtMixin):
         return device_dict
 
     # not for wsgi, but for service to creaste hosting device
-    def create_device_sync(self, context, template_id, kwargs):
+    def create_device_sync(self, context, template_id, kwargs,
+                           service_context):
         assert template_id is not None
         device = {
             'device': {
                 'template_id': template_id,
                 'kwargs': kwargs,
+                'service_context': service_context,
             },
         }
         device_dict = self._create_device(context, device)
@@ -323,3 +390,250 @@ class ServiceVMPlugin(vm_db.ServiceResourcePluginDb, ServiceVMMgmtMixin):
 
     def detach_interface(self, context, id, port_id):
         return self._do_interface(context, id, port_id, 'dettach_interface')
+
+    ###########################################################################
+    # logical service instance
+    #
+    def _create_service_instance_mgmt(
+            self, context, device_dict, service_instance_dict):
+        kwargs = {
+            mgmt_constants.KEY_ACTION: mgmt_constants.ACTION_CREATE_SERVICE,
+            mgmt_constants.KEY_KWARGS: {
+                'device': device_dict,
+                'service_instance': service_instance_dict,
+            },
+        }
+        self.mgmt_call(context, device_dict, kwargs)
+
+        mgmt_driver = self.mgmt_service_driver(
+            context, device_dict, service_instance_dict)
+        service_instance_dict['mgmt_driver'] = mgmt_driver
+        mgmt_address = self.mgmt_service_address(
+            context, device_dict, service_instance_dict)
+        service_instance_dict['mgmt_address'] = mgmt_address
+        LOG.debug(_('service_instance_dict '
+                    '%(service_instance_dict)s '
+                    'mgmt_driver %(mgmt_driver)s '
+                    'mgmt_address %(mgmt_address)s'),
+                  {'service_instance_dict':
+                   service_instance_dict,
+                   'mgmt_driver': mgmt_driver, 'mgmt_address': mgmt_address})
+        self._update_service_instance_mgmt(
+            context, service_instance_dict['id'], mgmt_driver, mgmt_address)
+
+        self.mgmt_service_create_pre(
+            context, device_dict, service_instance_dict)
+        self.mgmt_service_call(
+            context, device_dict, service_instance_dict, kwargs)
+
+    def _create_service_instance_db(self, context, device_id,
+                                    service_instance_param, managed_by_user):
+        return super(ServiceVMPlugin, self)._create_service_instance(
+            context, device_id, service_instance_param, managed_by_user)
+
+    def _create_service_instance_by_type(
+            self, context, device_dict,
+            name, service_type, service_table_id):
+        LOG.debug(_('device_dict %(device_dict)s '
+                    'service_type %(service_type)s'),
+                  {'device_dict': device_dict,
+                   'service_type': service_type})
+        service_type_id = [
+            s['id'] for s in
+            device_dict['device_template']['service_types']
+            if s['service_type'].upper() == service_type.upper()][0]
+
+        service_instance_param = {
+            'name': name,
+            'service_table_id': service_table_id,
+            'service_type': service_type,
+            'service_type_id': service_type_id,
+        }
+        service_instance_dict = self._create_service_instance_db(
+            context, device_dict['id'], service_instance_param, False)
+
+        new_status = constants.ACTIVE
+        try:
+            self._create_service_instance_mgmt(
+                context, device_dict, service_instance_dict)
+        except Exception:
+            new_status = constants.ERROR
+            raise
+        finally:
+            service_instance_dict['status'] = new_status
+            self.mgmt_service_create_post(
+                context, device_dict, service_instance_dict)
+            self._update_service_instance_post(
+                context, service_instance_dict['id'], new_status)
+        return service_instance_dict
+
+    # for service drivers. e.g. hosting_driver of loadbalancer
+    def create_service_instance_by_type(self, context, device_dict,
+                                        name, service_type, service_table_id):
+        self._update_device_pre(context, device_dict['id'])
+        new_status = constants.ACTIVE
+        try:
+            return self._create_service_instance_by_type(
+                context, device_dict, name, service_type,
+                service_table_id)
+        except Exception:
+            LOG.exception(_('create_service_instance_by_type'))
+            new_status = constants.ERROR
+        finally:
+            self._update_device_post(context, device_dict['id'], new_status)
+
+    def _create_service_instance_wait(self, context, device_id,
+                                      service_instance_dict):
+        device_dict = self.get_device(context, device_id)
+
+        new_status = constants.ACTIVE
+        try:
+            self._create_service_instance_mgmt(
+                context, device_dict, service_instance_dict)
+        except Exception:
+            LOG.exception(_('_create_service_instance_mgmt'))
+            new_status = constants.ERROR
+        service_instance_dict['status'] = new_status
+        self.mgmt_service_create_post(
+            context, device_dict, service_instance_dict)
+        self._update_service_instance_post(
+            context, service_instance_dict['id'], new_status)
+
+    # for service drivers. e.g. hosting_driver of loadbalancer
+    def _create_service_instance(self, context, device_id,
+                                 service_instance_param, managed_by_user):
+        service_instance_dict = self._create_service_instance_db(
+            context, device_id, service_instance_param, managed_by_user)
+        self.spawn_n(self._create_service_instance_wait, context,
+                     device_id, service_instance_dict)
+        return service_instance_dict
+
+    def create_service_instance(self, context, service_instance):
+        service_instance_param = service_instance['service_instance'].copy()
+        device = service_instance_param.pop('devices')
+        device_id = device[0]
+        service_instance_dict = self._create_service_instance(
+            context, device_id, service_instance_param, True)
+        return service_instance_dict
+
+    def _update_service_instance_wait(self, context, service_instance_dict,
+                                      mgmt_kwargs, callback, errorback):
+        devices = service_instance_dict['devices']
+        assert len(devices) == 1
+        device_dict = self.get_device(context, devices[0])
+        kwargs = {
+            mgmt_constants.KEY_ACTION: mgmt_constants.ACTION_UPDATE_SERVICE,
+            mgmt_constants.KEY_KWARGS: {
+                'device': device_dict,
+                'service_instance': service_instance_dict,
+                mgmt_constants.KEY_KWARGS: mgmt_kwargs,
+            }
+        }
+        try:
+            self.mgmt_call(context, device_dict, kwargs)
+            self.mgmt_service_update_pre(context, device_dict,
+                                         service_instance_dict)
+            self.mgmt_service_call(context, device_dict,
+                                   service_instance_dict, kwargs)
+        except Exception:
+            LOG.exception(_('mgmt call failed %s'), kwargs)
+            service_instance_dict['status'] = constants.ERROR
+            self.mgmt_service_update_post(context, device_dict,
+                                          service_instance_dict)
+            self._update_service_instance_post(
+                context, service_instance_dict['id'], constants.ERROR)
+            if errorback:
+                errorback()
+        else:
+            service_instance_dict['status'] = constants.ACTIVE
+            self.mgmt_service_update_post(context, device_dict,
+                                          service_instance_dict)
+            self._update_service_instance_post(
+                context, service_instance_dict['id'], constants.ACTIVE)
+            if callback:
+                callback()
+
+    # for service drivers. e.g. hosting_driver of loadbalancer
+    def _update_service_instance(self, context, service_instance_id,
+                                 mgmt_kwargs, callback, errorback):
+        service_instance_dict = self._update_service_instance_pre(
+            context, service_instance_id, {})
+        self.spawn_n(self._update_service_instance_wait, context,
+                     service_instance_dict, mgmt_kwargs, callback, errorback)
+
+    # for service drivers. e.g. hosting_driver of loadbalancer
+    def _update_service_table_instance(
+            self, context, service_table_id, mgmt_kwargs, callback, errorback):
+        _device_dict, service_instance_dict = self.get_by_service_table_id(
+            context, service_table_id)
+        service_instance_dict = self._update_service_instance_pre(
+            context, service_instance_dict['id'], {})
+        self.spawn_n(self._update_service_instance_wait, context,
+                     service_instance_dict, mgmt_kwargs, callback, errorback)
+
+    def update_service_instance(self, context, service_instance_id,
+                                service_instance):
+        mgmt_kwargs = service_instance['service_instance'].get('kwarg', {})
+        service_instance_dict = self._update_service_instance_pre(
+            context, service_instance_id, service_instance)
+
+        self.spawn_n(self._update_service_instance_wait, context,
+                     service_instance_dict, mgmt_kwargs, None, None)
+        return service_instance_dict
+
+    def _delete_service_instance_wait(self, context, device, service_instance,
+                                      mgmt_kwargs, callback, errorback):
+        service_instance_id = service_instance['id']
+        kwargs = {
+            mgmt_constants.KEY_ACTION: mgmt_constants.ACTION_DELETE_SERVICE,
+            mgmt_constants.KEY_KWARGS: {
+                'device': device,
+                'service_instance': service_instance,
+                mgmt_constants.KEY_KWARGS: mgmt_kwargs,
+            }
+        }
+        try:
+            self.mgmt_service_delete_pre(context, device, service_instance)
+            self.mgmt_service_call(context, device, service_instance, kwargs)
+            self.mgmt_call(context, device, kwargs)
+        except Exception:
+            LOG.exception(_('mgmt call failed %s'), kwargs)
+            service_instance['status'] = constants.ERROR
+            self.mgmt_service_delete_post(context, device, service_instance)
+            self._update_service_instance_post(context, service_instance_id,
+                                               constants.ERROR)
+            if errorback:
+                errorback()
+        else:
+            service_instance['status'] = constants.ACTIVE
+            self.mgmt_service_delete_post(context, device, service_instance)
+            self._delete_service_instance_post(context, service_instance_id)
+            if callback:
+                callback()
+
+    # for service drivers. e.g. hosting_driver of loadbalancer
+    def _delete_service_table_instance(
+            self, context, service_table_instance_id,
+            mgmt_kwargs, callback, errorback):
+        try:
+            device, service_instance = self.get_by_service_table_id(
+                context, service_table_instance_id)
+        except orm_exc.NoResultFound:
+            # there are no entry for some reason.
+            # e.g. partial creation due to error
+            callback()
+            return
+        self._delete_service_instance_pre(context, service_instance['id'],
+                                          False)
+        self.spawn_n(
+            self._delete_service_instance_wait, context, device,
+            service_instance, mgmt_kwargs, callback, errorback)
+
+    def delete_service_instance(self, context, service_instance_id):
+        # mgmt_kwargs is needed?
+        device, service_instance = self.get_by_service_instance_id(
+            context, service_instance_id)
+        self._delete_service_instance_pre(context, service_instance_id, True)
+        self.spawn_n(
+            self._delete_service_instance_wait, context, device,
+            service_instance, {}, None, None)
