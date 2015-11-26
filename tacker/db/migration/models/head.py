eeeb37ab2c8784c9025cbacd3da23cfedd110727
gong yong sheng <gong.yongsheng@99cloud.net>
2015-11-26 15:04:44 +0800

    Make tacker-db-manage revision 'xxx' --autogenerate work
    We have to make db model visible to tacker-db-manage for it
    to work well.
    
    Closes-bug: 1520092
    Change-Id: Ie97ee2d8db27856372c03b77b0942a888de778da

---
+++ tacker/db/migration/models/head.py
@@ -0,0 +1,30 @@
+# Copyright (c) 2014 OpenStack Foundation.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""
+The module provides all database models at current HEAD.
+
+Its purpose is to create comparable metadata with current database schema.
+Based on this comparison database can be healed with healing migration.
+
+"""
+
+from tacker.db import model_base
+from tacker.db.vm import proxy_db  # noqa
+from tacker.db.vm import vm_db  # noqa
+
+
+def get_metadata():
+    return model_base.BASE.metadata
