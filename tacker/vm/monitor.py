43660d01f296c18462405b8772a35d14a9b4a1b5
dharmendra <dharmendra.kushwaha@nectechnologies.in>
2016-02-02 09:59:39 +0530

    Removing the method re-defined in same class.
    Removing the redefinition of method in same class
    
    Change-Id: I8cdb6a54433f3b62264a9ec10318cdc8df923172

--- tacker/vm/monitor.py
+++ tacker/vm/monitor.py
@@ -205,10 +205,6 @@ class ActionPolicy(object):
     def get_supported_actions(cls):
         return cls._POLICIES.keys()
 
-    @abc.abstractmethod  # noqa
-    def execute_action(cls, plugin, device_dict):
-        pass
-
 
 @ActionPolicy.register('respawn')
 class ActionRespawn(ActionPolicy):
