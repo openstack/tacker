4a8d109839910aec0f97e5305e3788500fcafff5
Sripriya <sseetha@brocade.com>
2016-03-30 16:40:29 -0700

    Revert multiple mgmtdriver check for old templates
    Revert the changes done to handle multiple mgmt drivers for old
    templates. This is done to unblock the vnfd create issue
    containing mgmt driver, which errors out for the 'noop' and vdu
    mgmt driver mismatch in multiple mgmt driver check.
    
    Also add a new openwrt config file for tosca openwrt template
    
    Change-Id: I89b9312a926fcf720381a4c5d6420acbadad7bd7

--- tacker/vm/infra_drivers/heat/heat.py
+++ tacker/vm/infra_drivers/heat/heat.py
@@ -128,11 +128,7 @@ class DeviceHeat(abstract_driver.DeviceAbstractDriver):
             for vdu in vnfd_dict.get('vdus', {}).values():
                 mgmt_driver = vdu.get('mgmt_driver')
                 if mgmt_driver:
-                    if device_template_dict.get(
-                            'mgmt_driver', mgmt_driver) != mgmt_driver:
-                        raise vnfm.MultipleMGMTDriversSpecified()
-                    else:
-                        device_template_dict['mgmt_driver'] = mgmt_driver
+                    device_template_dict['mgmt_driver'] = mgmt_driver
         LOG.debug(_('device_template %s'), device_template)
 
     @log.log
