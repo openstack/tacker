9060d059742746fdc8877084a5abb8b89d6a7e7c
dharmendra <dharmendra.kushwaha@nectechnologies.in>
2016-08-29 18:31:26 +0900

    Cleaning utils code
    Removing unused code from common/utils.py
    
    Change-Id: Iffac3291d5ddd6c6d4b616f152679ad34f6330a5

--- tacker/tests/unit/test_common_utils.py
+++ tacker/tests/unit/test_common_utils.py
@@ -290,33 +290,6 @@ class TestParseVlanRangeList(UtilTestParseVlanRanges):
         self.assertEqual(expected_networks, self.parse_list(config_list))
 
 
-class TestDictUtils(base.BaseTestCase):
-    def test_dict2str(self):
-        dic = {"key1": "value1", "key2": "value2", "key3": "value3"}
-        expected = "key1=value1,key2=value2,key3=value3"
-        self.assertEqual(expected, utils.dict2str(dic))
-
-    def test_str2dict(self):
-        string = "key1=value1,key2=value2,key3=value3"
-        expected = {"key1": "value1", "key2": "value2", "key3": "value3"}
-        self.assertEqual(expected, utils.str2dict(string))
-
-    def test_dict_str_conversion(self):
-        dic = {"key1": "value1", "key2": "value2"}
-        self.assertEqual(dic, utils.str2dict(utils.dict2str(dic)))
-
-    def test_diff_list_of_dict(self):
-        old_list = [{"key1": "value1"},
-                    {"key2": "value2"},
-                    {"key3": "value3"}]
-        new_list = [{"key1": "value1"},
-                    {"key2": "value2"},
-                    {"key4": "value4"}]
-        added, removed = utils.diff_list_of_dict(old_list, new_list)
-        self.assertEqual([dict(key4="value4")], added)
-        self.assertEqual([dict(key3="value3")], removed)
-
-
 class _CachingDecorator(object):
     def __init__(self):
         self.func_retval = 'bar'
