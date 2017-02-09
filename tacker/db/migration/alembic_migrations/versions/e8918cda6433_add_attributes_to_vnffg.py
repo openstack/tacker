15659a8557d64f07d269e678a139789e7cd5cae1
Yan Xing'an <yanxingan@cmss.chinamobile.com>
2017-02-09 00:34:27 -0800

    Add column attributes to vnffgs in db.
    Change-Id: If6093180899b8965739ae3ba97483a2f2b658f91
    Implements: blueprint vnffgd-param-support

---
+++ tacker/db/migration/alembic_migrations/versions/e8918cda6433_add_attributes_to_vnffg.py
@@ -0,0 +1,38 @@
+# Copyright 2017 OpenStack Foundation
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+"""add_attributes_to_vnffg
+
+Revision ID: e8918cda6433
+Revises: 000632983ada
+Create Date: 2017-02-09 00:11:08.081746
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'e8918cda6433'
+down_revision = '000632983ada'
+
+from alembic import op
+import sqlalchemy as sa
+from tacker.db.types import Json
+
+
+def upgrade(active_plugins=None, options=None):
+    op.add_column('vnffgs', sa.Column('attributes', Json))
+
+
+def downgrade(active_plugins=None, options=None):
+    op.drop_column('vnffgs', 'attributes')
