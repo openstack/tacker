ea03d156e5e72a08c9a5d16fd048c555ea06305d
dineshbhor <dinesh.bhor@nttdata.com>
2017-12-28 15:48:49 +0900

    Remove unused constants
    There are constants defined in below module which are not used
    anywhere in the tacker code:
        tacker.common.constants.py
    
    This patch removes such unused constants.
    
    TrivialFix
    Change-Id: Ib76f5ca1735d66bdcf5c25503dcecc6b536b59d3

--- tacker/common/constants.py
+++ tacker/common/constants.py
@@ -16,32 +16,7 @@
 # TODO(salv-orlando): Verify if a single set of operational
 # status constants is achievable
 
-TYPE_BOOL = "bool"
-TYPE_INT = "int"
-TYPE_LONG = "long"
-TYPE_FLOAT = "float"
-TYPE_LIST = "list"
-TYPE_DICT = "dict"
-
 PAGINATION_INFINITE = 'infinite'
 
 SORT_DIRECTION_ASC = 'asc'
 SORT_DIRECTION_DESC = 'desc'
-
-# attribute name for nova boot
-ATTR_NAME_IMAGE = 'image'
-ATTR_NAME_FLAVOR = 'flavor'
-ATTR_NAME_META = 'meta'
-ATTR_NAME_FILES = "files"
-ATTR_NAME_RESERVEATION_ID = 'reservation_id'
-ATTR_NAME_SECURITY_GROUPS = 'security_groups'
-ATTR_NAME_USER_DATA = 'user_data'
-ATTR_NAME_KEY_NAME = 'key_name'
-ATTR_NAME_AVAILABILITY_ZONE = 'availability_zone'
-ATTR_NAME_BLOCK_DEVICE_MAPPING = 'block_device_mapping'
-ATTR_NAME_BLOCK_DEVICE_MAPPING_V2 = 'block_device_mapping_v2'
-ATTR_NAME_NICS = 'nics'
-ATTR_NAME_NIC = 'nic'
-ATTR_NAME_SCHEDULER_HINTS = 'sheculer_hints'
-ATTR_NAME_CONFIG_DRIVE = 'config_drive'
-ATTR_NAME_DISK_CONFIG = 'disk_config'
