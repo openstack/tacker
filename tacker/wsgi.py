704274e3848493643539557074b4fbbcd98b9262
Anusree <anusree.a04@gmail.com>
2018-07-25 03:54:09 +0530

    Enable mutable config in tacker
    New releases of oslo.config support a 'mutable' parameter to Opts.
    oslo.service provides an option here Icec3e664f3fe72614e373b2938e8dee53cf8bc5e
    allows services to tell oslo.service they want mutate_config_files to be
    called by passing a parameter.
    
    This commit is to use the same. This allows tacker to benefit from
    I1e7a69de169cc85f4c09954b2f46ce2da7106d90, where the 'debug' option
    (owned by oslo.log) is made mutable. we should be able to turn debug
    logging on and off by changing the config.
    
    tc goal:
    https://governance.openstack.org/tc/goals/rocky/enable-mutable-configuration.html
    
    Change-Id: I81df7830bbee2288938016c214495dfe2f90eec9

--- tacker/wsgi.py
+++ tacker/wsgi.py
@@ -228,8 +228,8 @@ class Server(object):
         else:
             # Minimize the cost of checking for child exit by extending the
             # wait interval past the default of 0.01s.
-            self._launcher = common_service.ProcessLauncher(CONF,
-                                                            wait_interval=1.0)
+            self._launcher = common_service.ProcessLauncher(
+                CONF, wait_interval=1.0, restart_method='mutate')
             self._server = WorkerService(self, application)
             self._launcher.launch_service(self._server, workers=workers)
 
